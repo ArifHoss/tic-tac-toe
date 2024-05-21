fc94fe0c9a1d2a051ef45e4a502ef49d25ded98f
Arif Hossain <hossaininfo@ymail.com>
2024-05-21 16:37:52 +0200

    now player name can edit dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
+import Player from "./components/Player.jsx";
 
 function App() {
-  
 
-  return (
-    <main>
-      <div id="game-container">
-      <ol id="players">
-        <li>
-          <span className="player-name">Player 01</span>
-          <span className="player-symbol">X</span>
-        </li>
-        <li>
-          <span className="player-name">Player 02</span>
-          <span className="player-symbol">O</span>
-        </li>
-      </ol>
-        GAME BOARD
-      </div>
-      LOG
-    </main>
-  )
+
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player initialName="Player-01" symbol="X"/>
+                    <Player initialName="Player-02" symbol="O"/>
+                    {/*<li>*/}
+                    {/*    <span className="player">*/}
+                    {/*        <span className="player-name">Player 01</span>*/}
+                    {/*        <span className="player-symbol">X</span>*/}
+                    {/*    </span>*/}
+                    {/*    <button>Edit</button>*/}
+                    {/*</li>*/}
+                    {/*<li>*/}
+                    {/*    <span className="player">*/}
+                    {/*        <span className="player-name">Player 02</span>*/}
+                    {/*        <span className="player-symbol">O</span>*/}
+                    {/*    </span>*/}
+                    {/*    <button>Edit</button>*/}
+                    {/*</li>*/}
+                </ol>
+                GAME BOARD
+            </div>
+            LOG
+        </main>
+    )
 }
 
 export default App
